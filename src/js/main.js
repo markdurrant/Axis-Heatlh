b29f3525078b2cff4a3857183a9d90a13251081f
Mark Durrant <mark.j.durrant.gmail.com>
2014-09-13 14:18:50 +0100

    add starting content

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
-(function () { 'use strict';
+// function outputUpdate(vol) {
+//   document.querySelector('#volume').value = vol;
+//   console.log( vol );
+// }
 
-}()); // end 'use strict'
+var      dotElm = $( '.dot' ),
+    yAxisSlider = $( '.y-axis-slider' ),
+    xAxisSlider = $( '.x-axis-slider' ),
+     posClasses = "y-pos-1 y-pos-2 y-pos-3 x-pos-1 x-pos-2 x-pos-3 x-pos-4";
+
+// apply position classes
+function dotPos ( ) {
+  dotElm.removeClass( posClasses );
+  dotElm.addClass( 'y-pos-' + yAxisSlider.val() );
+  dotElm.addClass( 'x-pos-' + xAxisSlider.val() );
+}
